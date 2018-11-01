fb04f78a5253903510ea8c426927da66f5337bbf
Muaath Alaraj <mistermo716@gmail.com>
2018-11-01 06:18:04 +0000

    neon tic tac toe

---
+++ hw/hw2/index.php
@@ -0,0 +1,22 @@
+<html>
+    
+    <head>
+        <title>Neon Tic Tac Toe</title>
+        <style>
+             @import url("css/main.css");
+        </style>
+    </head>
+
+    <body style="background-color: black">
+    <?php
+    include 'inc/functions.php';
+    setGame();
+    //echo "<img class='xo xo5' src='img/neono.png'/>";
+    
+    
+   
+
+    ?>
+    </body>
+    
+</html>
