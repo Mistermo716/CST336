9bd936952b61310104faaafc94ff275b5d41314f
Muaath Alaraj <mistermo716@gmail.com>
2018-11-03 20:21:30 +0000

    aws c9 trouble

--- hw/hw2/inc/functions.php
+++ hw/hw2/inc/functions.php
@@ -4,8 +4,5 @@ echo "<img class='h1' src='img/neonline.png' width='700vw'/>";
     echo "<img class='h2' src='img/neonline.png' width='700vw'/>";
     echo "<img class='v1' src='img/neonline.png' width='700vw'/>";
     echo "<img class='v2' src='img/neonline.png' width='700vw'/>";
-    for($i=0; $i<9; $i++){
-     echo "<input class='s s$i' type='submit' value='$i'>";   
-    }
 }
 ?>
--- hw/hw2/index.php
+++ hw/hw2/index.php
@@ -13,6 +13,59 @@
     setGame();
     //echo "<img class='xo xo5' src='img/neono.png'/>";
     
+    $row1 = array();
+    $row2 = array();
+    $row3 = array();
+    
+    while(true){
+    
+    
+    if(isset($_POST['s1'])){
+        $row1[1] = "X";
+    }
+    }
+    
+    
+    for($i = 0; $i < 3; $i++){
+        
+        if($row1[$i] == "X"){
+        echo "<img class='xo xo$i' src='img/neonx.png'/>";
+        }
+        
+        if($row1[$i] == "O"){
+        echo "<img class='xo xo$i' src='img/neono.png'/>";
+        }
+        
+        if($row2[$i] == "X"){
+            $pos = $i + 3;
+            echo "<img class='xo xo$pos' src='img/neonx.png'/>";
+        }
+        
+        if($row2[$i] == "O"){
+            $pos = $i + 3;
+            echo "<img class='xo xo$pos' src='img/neono.png'/>";
+        }
+        
+        if($row3[$i] == "X"){
+            $pos = $i + 6;
+            echo "<img class='xo xo$pos' src='img/neonx.png'/>";
+        }
+        
+        if($row3[$i] == "O"){
+            $pos = $i + 6;
+            echo "<img class='xo xo$pos' src='img/neono.png'/>";
+        }
+        
+    }
+    
+    
+    echo '<form action="index.php" method="post">';
+    for($i=0; $i<9; $i++){
+     echo "<input class='s s$i' type='submit' value='' name='s$i'>";   
+    }
+    
+    echo'</form>';
+    
     
    
 
