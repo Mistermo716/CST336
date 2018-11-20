c9622cdc5eb4b97a27373196639a5da4795705cd
Muaath Alaraj <mistermo716@gmail.com>
2018-11-20 07:19:51 +0000

    lab6 complete bootstrap

--- labs/lab6/updateProduct.php
+++ labs/lab6/updateProduct.php
@@ -64,6 +64,7 @@ function getCategories($catId){
 <!DOCTYPE html>
 <html>
   <head>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title>Update Product</title>
   </head>
   
